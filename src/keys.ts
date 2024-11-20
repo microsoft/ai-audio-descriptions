dd9332c8a441825ee331f078cd579a9e3fd3287c
Saqib Shaikh <saqibs@microsoft.com>
2024-11-20 09:34:10 -0800

    Updating storage container name to match readme

--- src/keys.ts
+++ src/keys.ts
@@ -1,7 +1,7 @@
 const storageAccount: string = import.meta.env.VITE_STORAGE_ACCOUNT;
 const blobUri = `https://${storageAccount}.blob.core.windows.net`;
 const blobSasToken: string = import.meta.env.VITE_BLOB_SAS_TOKEN;
-const STORAGE_CONTAINER_NAME = "audio-descriptions";
+const STORAGE_CONTAINER_NAME = "audio-description";
 
 const aiServicesResource = import.meta.env.VITE_AI_SERVICES_RESOURCE;
 const aiServicesKey = import.meta.env.VITE_AI_SERVICES_KEY;
